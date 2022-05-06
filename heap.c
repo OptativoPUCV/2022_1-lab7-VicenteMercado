4b8eb4bb12e2ee3e62f6065401b2bb6e14c3101b
Your Name <you@example.com>
2022-05-06 17:19:54 -0400

    new try

--- heap.c
+++ heap.c
@@ -86,7 +86,7 @@ void heap_pop(Heap* pq){
          continue;
       } else break;
 
-      printf("size = %i, top = %i\n", posSup, pq->heapArray[0].priority);
+      printf("size = %i, top = %i\n", (int)posSup, (int)pq->heapArray[0].priority);
 
    }
 
