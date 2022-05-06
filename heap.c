dd2d2b372aa93adebbf4b465b31fa938ad9bc0a6
Your Name <you@example.com>
2022-05-06 17:36:28 -0400

    new try

--- heap.c
+++ heap.c
@@ -59,7 +59,7 @@ void heap_pop(Heap* pq){
    }
    heapElem* elemAux = (heapElem*)malloc(sizeof(heapElem));
 
-   pq->heapArray[0] = pq->heapArray[pq->size];
+   pq->heapArray[0] = pq->heapArray[pq->size-1];
    pq->size--;
    printf("Nuevo mayor prioridad: %d\n", (int)pq->heapArray[0].priority);
 
